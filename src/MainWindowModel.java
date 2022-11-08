27433e74846b36a286b53ebf2dd93348fd2610be
Getaji <t.tanaka.onion@gmail.com>
2022-11-08 20:20:08 +0900

    fix: モデル構成を一旦削除

--- src/MainWindowModel.java
+++ src/MainWindowModel.java
@@ -1,13 +1,4 @@
 import javafx.beans.property.SimpleStringProperty;
 
 public class MainWindowModel {
-    private final SimpleStringProperty message = new SimpleStringProperty("");
-
-    public String getMessage() {
-        return message.get();
-    }
-
-    public SimpleStringProperty messageProperty() {
-        return message;
-    }
 }
