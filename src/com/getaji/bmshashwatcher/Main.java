b199edb784a8d3760746b82e51c650d6befda46c
Getaji <t.tanaka.onion@gmail.com>
2022-11-13 13:10:52 +0900

    version: 0.3.2

--- src/com/getaji/bmshashwatcher/Main.java
+++ src/com/getaji/bmshashwatcher/Main.java
@@ -33,7 +33,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.function.Consumer;
 
 public class Main extends Application {
-    public static final String APP_VERSION = "0.3.1";
+    public static final String APP_VERSION = "0.3.2";
 
     private static Main INSTANCE;
 
